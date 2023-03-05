336a7c228aa53cb9afa98d7e3cda1d3ca31a4a9a
tatyana <deniscapin@yandex.ru>
2023-03-05 10:55:02 +0300

    Modules

--- src/index.js
+++ src/index.js
@@ -7,38 +7,30 @@ import './index.scss';
 import Swiper, {Thumbs, Scrollbar} from 'swiper';
 import 'swiper/css';
 import 'swiper/css/scrollbar';
-import {pagination} from "./modules/pagination";
-
-const paginationWrapper = document.querySelector('.pagination');
-
-const pageURL = new URL(location);
-const page = +pageURL.searchParams.get('page') || 1;
-
-let isMobile = false;
-const startPagination = () => {
-  if (window.innerWidth <= 560) {
-    pagination(paginationWrapper, 12, page, 4);
-    isMobile = true;
-  }else {
-    pagination(paginationWrapper, 12, page, 6);
-    isMobile = false;
-  }
-}
+import {startPagination} from "./modules/pagination";
+import {getGoods} from "./modules/goodsService";
+import {renderGoods} from "./modules/renderGoods";
 
 try {
-  startPagination();
+  const paginationWrapper = document.querySelector('.pagination');
+  const goodsList = document.querySelector('.goods__list')
 
-  window.addEventListener('resize', () => {
-    if (window.innerWidth <= 560 && !isMobile) {
-      pagination(paginationWrapper, 12, page, 4);
-      isMobile = true;
-    }
+  const pageURL = new URL(location);
+  const page = +pageURL.searchParams.get('page') || 1;
 
-    if (window.innerWidth > 560 && isMobile) {
-      pagination(paginationWrapper, 12, page, 6);
-      isMobile = false;
-    }
+  goodsList.innerHTML = `
+ <div class="goods__preload">
+<svg width="220" height="220" viewBox="0 0 220 220" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M184.667 6.6665V57.5236M311.81 57.5236L273.667 95.6665M57.5241 57.5236L95.667 95.6665M184.667 362.667V311.809M311.81 311.809L273.667 273.667M57.5241 311.809L95.667 273.667M6.66699 184.667H57.5241M311.81 184.667H362.667" stroke="#FFC700" stroke-width="12.1905" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
+
+</div>
+  `;
+  getGoods({page}).then(({goods, pages, page}) => {
+    renderGoods(goodsList, goods);
+    startPagination(paginationWrapper, pages, page);
   })
+
 
 } catch (e) {
   console.warn(e);
